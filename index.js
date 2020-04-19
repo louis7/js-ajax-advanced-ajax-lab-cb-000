b81a841ccf9c9bf95cef78e18ae1f4d4af59c586
louis baptiste <louisvis@ymail.com>
2020-04-19 00:18:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function showRepositories() {
+  const repos = JSON.parse(this.responseText);
+  const repoList = `<ul>${repos
+    .map(r => '<li>' + r.name + '</li>')
+    .join('')}</ul>`;
+  document.getElementById('repositories').innerHTML = repoList;
+}
+
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showRepositories);
+  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.send();
+}
